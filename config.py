9af561d14eb8ea944d44bf70b768d8b693b29b57
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-06-06 18:05:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,29 +12,29 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Specify where to get the following credentials
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BTW_AYU_0")
-BOT_USERNAME = getenv("BOT_USERNAME", "SUKKU_X_Music_Bot")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Farzicoderxd")
+BOT_USERNAME = getenv("BOT_USERNAME", "SWEETY_MUSIC_BOT")
 BOT_NAME = getenv("BOT_NAME", "˹𝙺ɪsʜᴜ ✘ 𝙼ᴜsɪᴄ˼")
 ASSUSERNAME = getenv("ASSUSERNAME", "SUKKU_Assis")
-EVALOP = list(map(int, getenv("EVALOP", "6797202080").split()))
+EVALOP = list(map(int, getenv("EVALOP", "5569528945").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOGGER_ID = int(getenv("LOGGER_ID", -1002078835966))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002150699990))
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
 PLAYHT_API = getenv("PLAYHT_API")
-OWNER_ID = int(getenv("OWNER_ID", 7391708422))
+OWNER_ID = int(getenv("OWNER_ID", 5569528945))
 
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NOBITA018/KISHU-X-MUSIC/edit/master/config.py")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Sumitmeena12/KISHU-X-MUSIC/edit/master/config.py")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 # Support and contact information - Provide your own support channels
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NOBITA_PROFESY")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NOBITA_PROFESY")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dragondpworld")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/apnihavelixd")
 
 # Server limits and configurations - These can be set based on your server configurations
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
@@ -81,10 +81,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cd02e62dea09e7514c45f.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/163bfac7aed8a92c47d0d.jpg"
 )
 PING_VID_URL = getenv(
-    "PING_VID_URL", "https://telegra.ph/file/8eec2dacdc0c10c2d692e.mp4"
+    "PING_VID_URL", "https://telegra.ph/file/163bfac7aed8a92c47d0d.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/164d3363f4775c9a5e0e2.jpg"
 STATS_VID_URL = "https://telegra.ph/file/8608f97ed3d5dab884ba5.mp4"
